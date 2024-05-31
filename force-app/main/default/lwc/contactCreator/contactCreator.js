38fcf64388918afc8d3ac712071bb44cee72d2a9
zhaoxue <fromzhaoxue@gmail.com>
2024-05-31 15:45:42 +0900

    update contactCreator-create data

--- force-app/main/default/lwc/contactCreator/contactCreator.js
+++ force-app/main/default/lwc/contactCreator/contactCreator.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
-
-export default class ContactCreator extends LightningElement {}
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import Contact_OBJECT from '@salesforce/schema/Contact';
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+export default class ContactCreator extends LightningElement {
+    objectApiName = Contact_OBJECT;
+    fields = [FIRSTNAME_FIELD,LASTNAME_FIELD, EMAIL_FIELD];
+    handleSuccess(event) {
+        const toastEvent = new ShowToastEvent({
+            title: "Contact created",
+            message: "Record ID: " + event.detail.id,
+            variant: "success"
+        });
+        this.dispatchEvent(toastEvent);
+    }
+}
