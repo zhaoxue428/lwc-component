1bd2ce7968a7472fc9c13cc24794f84a92dba8fe
zhaoxue <fromzhaoxue@gmail.com>
2024-05-31 16:25:44 +0900

    contactList002

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
-
-export default class ContactList extends LightningElement {}
+import { LightningElement, wire } from 'lwc';
+import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
+import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+import getContacts from '@salesforce/apex/ContactController.getContacts';
+const COLUMNS = [
+    { label: 'FirstName', fieldName: FIRSTNAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'LastName', fieldName: LASTNAME_FIELD.fieldApiName, type: 'text' },
+    { label: 'Email', fieldName: EMAIL_FIELD.fieldApiName, type: 'text' }
+];
+export default class ContactList extends LightningElement {
+    columns = COLUMNS;
+    @wire(getContacts)
+    contacts;
+    get errors() {
+        return (this.contacts.error) ?
+            reduceErrors(this.contacts.error) : [];
+    }
+}
